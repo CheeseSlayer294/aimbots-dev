18bddea444df423ab7d07915f2b98892c842997b
jpostelnik <jonathan@postelnik.net>
2023-06-07 19:28:41 +0100

    added another watch command

--- aimbots-src/src/subsystems/hopper/hopper.cpp
+++ aimbots-src/src/subsystems/hopper/hopper.cpp
@@ -42,13 +42,13 @@ bool HopperSubsystem::isHopperReady() const {
     // the delay is mostly just to keep commands from ending b4 they should, bc isRampTargetMet() is based on pwm ramp finishing
 }
 
-uint8_t state = 0;
+uint8_t state;
 uint8_t HopperSubsystem::getHopperState() const {
     state = hopper_state;
     return hopper_state;
 }
 
-uint8_t new_state_display = 0;
+uint8_t new_state_display;
 void HopperSubsystem::setHopperState(uint8_t new_state) {
     new_state_display = new_state;
     hopper_state = new_state;
