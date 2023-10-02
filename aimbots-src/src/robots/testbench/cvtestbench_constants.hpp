8937a214bef30c91a2cccd58c37b85ad7e226dbf
HeroOfWind243 <67348400+HeroOfWind243@users.noreply.github.com>
2023-10-02 11:33:23 -0500

    Initial Testbench Setup (still need to blank out everything)

--- aimbots-src/src/robots/testbench/cvtestbench_constants.hpp
+++ aimbots-src/src/robots/testbench/cvtestbench_constants.hpp
@@ -2,13 +2,11 @@
 #include "utils/common_types.hpp"
 #include "utils/math/matrix_helpers.hpp"
 
-#define GIMBAL_UNTETHERED
+#define FEEDER_COMPATIBLE
+#define SHOOTER_COMPATIBLE
+#define GIMBAL_COMPATIBLE
 
-/**
- * @brief Defines the number of motors created for the chassis.
- */
-static constexpr uint8_t DRIVEN_WHEEL_COUNT = 4;
-static constexpr uint8_t MOTORS_PER_WHEEL = 1;
+#define GIMBAL_UNTETHERED
 
 static constexpr uint8_t SHOOTER_MOTOR_COUNT = 2;
 
@@ -138,11 +136,6 @@ static constexpr SmoothPIDConfig PITCH_VELOCITY_PID_CONFIG = {
     .errorDerivativeFloor = 0.0f,
 };
 
-static constexpr float CHASSIS_VELOCITY_YAW_LOAD_FEEDFORWARD = 1.0f;
-static constexpr float CHASSIS_VELOCITY_PITCH_LOAD_FEEDFORWARD = 1.0f;
-
-static constexpr float CHASSIS_LINEAR_ACCELERATION_PITCH_COMPENSATION = 0.0f;
-
 // clang-format off
 const modm::Pair<float, float> YAW_FEEDFORWARD_VELOCITIES[11] = {
                                                                     {0.0f, 0.0f},
@@ -176,25 +169,16 @@ const modm::Pair<float, float> PITCH_FEEDFORWARD_VELOCITIES[11] = {
 const modm::interpolation::Linear<modm::Pair<float, float>> YAW_VELOCITY_FEEDFORWARD(YAW_FEEDFORWARD_VELOCITIES, 11);
 const modm::interpolation::Linear<modm::Pair<float, float>> PITCH_VELOCITY_FEEDFORWARD(PITCH_FEEDFORWARD_VELOCITIES, 11);
 
+static constexpr float CHASSIS_VELOCITY_YAW_LOAD_FEEDFORWARD = 1.0f;
+static constexpr float CHASSIS_VELOCITY_PITCH_LOAD_FEEDFORWARD = 1.0f;
+
+static constexpr float CHASSIS_LINEAR_ACCELERATION_PITCH_COMPENSATION = 0.0f;
+
 static constexpr float kGRAVITY = 0.0f;
 static constexpr float HORIZON_OFFSET = -0.0f;
 // -------------------------------------------------------------------------------------------------------------------------
 
 static Vector3f IMU_MOUNT_POSITION{0.0f, 0.0f, 0.0f};
-
-static constexpr SmoothPIDConfig CHASSIS_VELOCITY_PID_CONFIG = {
-    .kp = 5.0f,
-    .ki = 0.0f,
-    .kd = 0.5f,
-    .maxICumulative = 10.0f,
-    .maxOutput = M3508_MAX_OUTPUT,
-    .tQDerivativeKalman = 1.0f,
-    .tRDerivativeKalman = 1.0f,
-    .tQProportionalKalman = 1.0f,
-    .tRProportionalKalman = 1.0f,
-    .errDeadzone = 0.0f,
-    .errorDerivativeFloor = 0.0f,
-};
 
 static constexpr SmoothPIDConfig FEEDER_VELOCITY_PID_CONFIG = {
     .kp = 15.0f,
@@ -224,9 +208,6 @@ static constexpr SmoothPIDConfig SHOOTER_VELOCITY_PID_CONFIG = {
     .errorDerivativeFloor = 0.0f,
 };
 
-// 1 for no symmetry, 2 for 180 degree symmetry, 4 for 90 degree symmetry
-static constexpr uint8_t CHASSIS_SNAP_POSITIONS = 1;
-
 // clang-format off
 static constexpr uint16_t shooter_speed_array[6] = {
     15, 3900,  // {ball m/s, flywheel rpm}
@@ -237,15 +218,8 @@ static constexpr uint16_t shooter_speed_array[6] = {
 static const Matrix<uint16_t, 3, 2> SHOOTER_SPEED_MATRIX(shooter_speed_array);
 
 static constexpr float FEEDER_DEFAULT_RPM = 3000.0f;
-static constexpr int DEFAULT_BURST_LENGTH = 5;  // balls
 
-// CAN Bus 2
-static constexpr CANBus CHASSIS_BUS = CANBus::CAN_BUS2;
-
-static constexpr MotorID LEFT_BACK_WHEEL_ID = MotorID::MOTOR1;
-static constexpr MotorID LEFT_FRONT_WHEEL_ID = MotorID::MOTOR2;
-static constexpr MotorID RIGHT_FRONT_WHEEL_ID = MotorID::MOTOR3;
-static constexpr MotorID RIGHT_BACK_WHEEL_ID = MotorID::MOTOR4;
+static constexpr float DEFAULT_BURST_LENGTH = 5;
 
 // CAN Bus 1
 static constexpr CANBus SHOOTER_BUS = CANBus::CAN_BUS1;
@@ -262,93 +236,14 @@ static constexpr bool SHOOTER_2_DIRECTION = true;
 
 static constexpr bool FEEDER_DIRECTION = false;
 
-// Hopper constants
-static constexpr tap::gpio::Pwm::Pin HOPPER_PIN = tap::gpio::Pwm::C1;
-
-static constexpr float HOPPER_PWM_RAMP_SPEED = 0.01f;  // pwm percent per millisecond
-
-static constexpr float HOPPER_MIN_PWM = DS3218_MIN_PWM;
-static constexpr float HOPPER_MAX_PWM = DS3218_MAX_PWM;
-
-static constexpr float HOPPER_MIN_ANGLE = 0.0f;
-static constexpr float HOPPER_MAX_ANGLE = 270.0f;
-
-static constexpr float HOPPER_OPEN_ANGLE = 60.0f;
-static constexpr float HOPPER_CLOSED_ANGLE = 155.0f;
-
-static constexpr uint32_t HOPPER_MIN_ACTION_DELAY = 1000;  // Minimum time in ms between hopper lid flips
-
-// Mechanical chassis constants, all in m
-/**
- * Radius of the wheels (m).
- */
-static constexpr float WHEEL_RADIUS = 0.076;
-
-static constexpr float WHEELBASE_WIDTH = 0.366f;
-
-static constexpr float WHEELBASE_LENGTH = 0.366f;
-
 static constexpr float GIMBAL_X_OFFSET = 0.0f;
 static constexpr float GIMBAL_Y_OFFSET = 0.0f;
-
-static constexpr float CHASSIS_GEARBOX_RATIO = (1.0f / 19.0f);
 
 // Power limiting constants, will explain later
 static constexpr float POWER_LIMIT_SAFETY_FACTOR = 0.85f;
 static constexpr float STARTING_ENERGY_BUFFER = 60.0f;
 static constexpr float ENERGY_BUFFER_LIMIT_THRESHOLD = 60.0f;
 static constexpr float ENERGY_BUFFER_CRIT_THRESHOLD = 10.0f;
-
-/**
- * @brief Power constants for chassis
- */
-static constexpr int MIN_WHEEL_SPEED_SINGLE_MOTOR = 4000;
-static constexpr int MAX_WHEEL_SPEED_SINGLE_MOTOR = 8000;
-static constexpr int MIN_CHASSIS_POWER = 40;
-static constexpr int MAX_CHASSIS_POWER = 120;
-static constexpr int WHEEL_SPEED_OVER_CHASSIS_POWER_SLOPE =
-    (MAX_WHEEL_SPEED_SINGLE_MOTOR - MIN_WHEEL_SPEED_SINGLE_MOTOR) / (MAX_CHASSIS_POWER - MIN_CHASSIS_POWER);
-static_assert(WHEEL_SPEED_OVER_CHASSIS_POWER_SLOPE >= 0);
-
-/**
- * @brief Behavior constants for chassis
- */
-
-/**
- * The minimum desired wheel speed for chassis rotation, measured in RPM before
- * we start slowing down translational speed.
- */
-static constexpr float MIN_ROTATION_THRESHOLD = 800.0f;
-
-static constexpr float FOLLOW_GIMBAL_ANGLE_THRESHOLD = modm::toRadian(20.0f);
-
-static constexpr SmoothPIDConfig ROTATION_POSITION_PID_CONFIG = {
-    .kp = 1.25f,
-    .ki = 0.0f,
-    .kd = 0.00625f,
-    .maxICumulative = 10.0f,
-    .maxOutput = 1.0f,
-    .tQDerivativeKalman = 1.0f,
-    .tRDerivativeKalman = 1.0f,
-    .tQProportionalKalman = 1.0f,
-    .tRProportionalKalman = 1.0f,
-    .errDeadzone = 0.0f,
-    .errorDerivativeFloor = 0.0f,
-};
-
-/**
- * @brief TOKYO CONSTANTS
- */
-// Fraction that user input is multiplied by when "drifting"
-static constexpr float TOKYO_TRANSLATIONAL_SPEED_MULTIPLIER = 0.6f;
-// Fraction of the maximum translation speed for when rotation speed should be reduced
-static constexpr float TOKYO_TRANSLATION_THRESHOLD_TO_DECREASE_ROTATION_SPEED = 0.5f;
-// Fraction of max chassis speed applied to rotation speed
-static constexpr float TOKYO_ROTATIONAL_SPEED_FRACTION_OF_MAX = 0.75f;
-// Fraction to cut rotation speed by when the robot is "drifting"
-static constexpr float TOKYO_ROTATIONAL_SPEED_MULTIPLIER_WHEN_TRANSLATING = 0.7f;
-// Rotational speed increment per iteration to apply until rotation setpoint is reached
-static constexpr float TOKYO_ROTATIONAL_SPEED_INCREMENT = 50.0f;  // rpm
 
 /**
  * @brief Transformation Matrices, specific to robot
--- aimbots-src/src/robots/testbench/cvtestbench_control.cpp
+++ aimbots-src/src/robots/testbench/cvtestbench_control.cpp
@@ -17,46 +17,18 @@
 #include "tap/control/setpoint/commands/calibrate_command.hpp"
 #include "tap/control/toggle_command_mapping.hpp"
 //
-#include "subsystems/chassis/chassis.hpp"
-#include "subsystems/chassis/chassis_manual_drive_command.hpp"
-#include "subsystems/chassis/chassis_shakira_command.hpp"
-#include "subsystems/chassis/chassis_toggle_drive_command.hpp"
-#include "subsystems/chassis/chassis_tokyo_command.hpp"
-//
 #include "subsystems/feeder/feeder.hpp"
 #include "subsystems/feeder/full_auto_feeder_command.hpp"
 #include "subsystems/feeder/stop_feeder_command.hpp"
-//
-#include "subsystems/gimbal/controllers/gimbal_chassis_relative_controller.hpp"
-#include "subsystems/gimbal/controllers/gimbal_field_relative_controller.hpp"
-#include "subsystems/gimbal/gimbal.hpp"
-#include "subsystems/gimbal/gimbal_chase_command.hpp"
-#include "subsystems/gimbal/gimbal_control_command.hpp"
-#include "subsystems/gimbal/gimbal_field_relative_control_command.hpp"
 //
 #include "subsystems/shooter/brake_shooter_command.hpp"
 #include "subsystems/shooter/run_shooter_command.hpp"
 #include "subsystems/shooter/shooter.hpp"
 #include "subsystems/shooter/stop_shooter_command.hpp"
 #include "subsystems/shooter/stop_shooter_comprised_command.hpp"
-//
-#include "subsystems/hopper/close_hopper_command.hpp"
-#include "subsystems/hopper/hopper.hpp"
-#include "subsystems/hopper/open_hopper_command.hpp"
-#include "subsystems/hopper/toggle_hopper_command.hpp"
-//
-#include "subsystems/gui/gui_display.hpp"
-#include "subsystems/gui/gui_display_command.hpp"
-//
-#include "subsystems/barrel_manager/barrel_manager.hpp"
-#include "subsystems/barrel_manager/barrel_swap_command.hpp"
 
-using namespace src::Chassis;
 using namespace src::Feeder;
-using namespace src::Gimbal;
 using namespace src::Shooter;
-using namespace src::Hopper;
-using namespace src::GUI;
 
 /*
  * NOTE: We are using the DoNotUse_getDrivers() function here
@@ -74,83 +46,19 @@ namespace CVTestbenchControl {
 
 BarrelID currentBarrel = BarrelID::TURRET_17MM_1;
 
-src::Utils::RefereeHelperTurreted refHelper(drivers(), currentBarrel);
+src::Utils::RefereeHelperTurreted refHelper(drivers(), currentBarrel, 10);
 
 // Define subsystems here ------------------------------------------------
-ChassisSubsystem chassis(drivers());
 FeederSubsystem feeder(drivers());
-GimbalSubsystem gimbal(drivers());
 ShooterSubsystem shooter(drivers(), &refHelper);
-HopperSubsystem hopper(
-    drivers(),
-    HOPPER_PIN,
-    HOPPER_MAX_PWM,
-    HOPPER_MIN_PWM,
-    HOPPER_PWM_RAMP_SPEED,
-    HOPPER_MIN_ANGLE,
-    HOPPER_MAX_ANGLE,
-    HOPPER_MIN_ACTION_DELAY);
-
-// Robot Specific Controllers ------------------------------------------------
-GimbalChassisRelativeController gimbalChassisRelativeController(&gimbal);
-GimbalFieldRelativeController gimbalFieldRelativeController(drivers(), &gimbal);
 
 // Ballistics Solver -------------------------------------------------------
 src::Utils::Ballistics::BallisticsSolver ballisticsSolver(drivers(), BARREL_POSITION_FROM_GIMBAL_ORIGIN);
 
 // Configs -----------------------------------------------------------------
-SnapSymmetryConfig defaultSnapConfig = {
-    .numSnapPositions = CHASSIS_SNAP_POSITIONS,
-    .snapAngle = modm::toRadian(0.0f),
-};
 
-TokyoConfig defaultTokyoConfig = {
-    .translationalSpeedMultiplier = 0.6f,
-    .translationThresholdToDecreaseRotationSpeed = 0.5f,
-    .rotationalSpeedFractionOfMax = 0.75f,
-    .rotationalSpeedMultiplierWhenTranslating = 0.7f,
-    .rotationalSpeedIncrement = 50.0f,
-};
-
-SpinRandomizerConfig randomizerConfig = {
-    .minSpinRateModifier = 0.75f,
-    .maxSpinRateModifier = 1.0f,
-    .minSpinRateModifierDuration = 500,
-    .maxSpinRateModifierDuration = 3000,
-};
 
 // Define commands here ---------------------------------------------------
-ChassisManualDriveCommand chassisManualDriveCommand(drivers(), &chassis);
-ChassisToggleDriveCommand chassisToggleDriveCommand(drivers(), &chassis, &gimbal);
-ChassisTokyoCommand chassisTokyoCommand(drivers(), &chassis, &gimbal, defaultTokyoConfig, 0, false, randomizerConfig);
-// ChassisShakiraCommand chassisShakiraCommand(
-//     drivers(),
-//     &chassis,
-//     &gimbal,
-//     &ROTATION_POSITION_PID_CONFIG,
-//     &ballisticsSolver,
-//     &refHelper,
-//     4,
-//     modm::toRadian(45.0f),
-//     3000);
-
-GimbalControlCommand gimbalControlCommand(drivers(), &gimbal, &gimbalChassisRelativeController);
-GimbalFieldRelativeControlCommand gimbalFieldRelativeControlCommand(drivers(), &gimbal, &gimbalFieldRelativeController);
-GimbalFieldRelativeControlCommand gimbalFieldRelativeControlCommand2(drivers(), &gimbal, &gimbalFieldRelativeController);
-GimbalChaseCommand gimbalChaseCommand(
-    drivers(),
-    &gimbal,
-    &gimbalFieldRelativeController,
-    &refHelper,
-    &ballisticsSolver,
-    SHOOTER_SPEED_MATRIX[0][0]);
-GimbalChaseCommand gimbalChaseCommand2(
-    drivers(),
-    &gimbal,
-    &gimbalFieldRelativeController,
-    &refHelper,
-    &ballisticsSolver,
-    SHOOTER_SPEED_MATRIX[0][0]);
 FullAutoFeederCommand runFeederCommand(drivers(), &feeder, &refHelper, FEEDER_DEFAULT_RPM, 0.80f);
 FullAutoFeederCommand runFeederCommandFromMouse(drivers(), &feeder, &refHelper, FEEDER_DEFAULT_RPM, 0.80f);
 StopFeederCommand stopFeederCommand(drivers(), &feeder);
@@ -159,40 +67,16 @@ RunShooterCommand runShooterCommand(drivers(), &shooter, &refHelper);
 RunShooterCommand runShooterWithFeederCommand(drivers(), &shooter, &refHelper);
 StopShooterComprisedCommand stopShooterComprisedCommand(drivers(), &shooter);
 
-OpenHopperCommand openHopperCommand(drivers(), &hopper, HOPPER_OPEN_ANGLE);
-OpenHopperCommand openHopperCommand2(drivers(), &hopper, HOPPER_OPEN_ANGLE);
-CloseHopperCommand closeHopperCommand(drivers(), &hopper, HOPPER_CLOSED_ANGLE);
-CloseHopperCommand closeHopperCommand2(drivers(), &hopper, HOPPER_CLOSED_ANGLE);
-ToggleHopperCommand toggleHopperCommand(drivers(), &hopper, HOPPER_CLOSED_ANGLE, HOPPER_OPEN_ANGLE);
-
 // Define command mappings here -------------------------------------------
-HoldCommandMapping leftSwitchMid(
-    drivers(),  // gimbalFieldRelativeControlCommand
-    {/*&chassisToggleDriveCommand,*/ &gimbalChaseCommand},
-    RemoteMapState(Remote::Switch::LEFT_SWITCH, Remote::SwitchState::MID));
 
-// Enables both chassis and gimbal control and closes hopper
-HoldCommandMapping leftSwitchUp(
-    drivers(),  // gimbalFieldRelativeControlCommand2
-    {/*&chassisTokyoCommand,*/ &gimbalFieldRelativeControlCommand2},
-    RemoteMapState(Remote::Switch::LEFT_SWITCH, Remote::SwitchState::UP));
-
-// opens hopper
-HoldCommandMapping rightSwitchDown(
-    drivers(),
-    {&openHopperCommand},
-    RemoteMapState(Remote::Switch::RIGHT_SWITCH, Remote::SwitchState::DOWN));
-
-// Runs shooter only and closes hopper
 HoldCommandMapping rightSwitchMid(
     drivers(),
-    {&runShooterCommand, &closeHopperCommand},
+    {&runShooterCommand},
     RemoteMapState(Remote::Switch::RIGHT_SWITCH, Remote::SwitchState::MID));
 
-// Runs shooter with feeder and closes hopper
 HoldRepeatCommandMapping rightSwitchUp(
     drivers(),
-    {&runFeederCommand, &runShooterWithFeederCommand, &closeHopperCommand2},
+    {&runFeederCommand, &runShooterWithFeederCommand},
     RemoteMapState(Remote::Switch::RIGHT_SWITCH, Remote::SwitchState::UP),
     true);
 
@@ -208,22 +92,14 @@ HoldCommandMapping leftClickMouse(
 
 // Register subsystems here -----------------------------------------------
 void registerSubsystems(src::Drivers *drivers) {
-    drivers->commandScheduler.registerSubsystem(&chassis);
     drivers->commandScheduler.registerSubsystem(&feeder);
-    drivers->commandScheduler.registerSubsystem(&gimbal);
     drivers->commandScheduler.registerSubsystem(&shooter);
-    drivers->commandScheduler.registerSubsystem(&hopper);
-
-    drivers->kinematicInformant.registerSubsystems(&gimbal, &chassis);
 }
 
 // Initialize subsystems here ---------------------------------------------
 void initializeSubsystems() {
-    chassis.initialize();
     feeder.initialize();
-    gimbal.initialize();
     shooter.initialize();
-    hopper.initialize();
 }
 
 // Set default command here -----------------------------------------------
@@ -243,11 +119,8 @@ void startupCommands(src::Drivers *) {
 
 // Register IO mappings here -----------------------------------------------
 void registerIOMappings(src::Drivers *drivers) {
-    drivers->commandMapper.addMap(&leftSwitchUp);
-    drivers->commandMapper.addMap(&leftSwitchMid);
     drivers->commandMapper.addMap(&rightSwitchUp);
     drivers->commandMapper.addMap(&rightSwitchMid);
-    drivers->commandMapper.addMap(&rightSwitchDown);
     drivers->commandMapper.addMap(&leftClickMouse);
 }
 
