c0e1373a8b927cd5e3eb414eb2118edc75245459
HeroOfWind243 <67348400+HeroOfWind243@users.noreply.github.com>
2023-10-02 11:45:09 -0500

    Tweaked constants more

--- aimbots-src/src/robots/testbench/cvtestbench_constants.hpp
+++ aimbots-src/src/robots/testbench/cvtestbench_constants.hpp
@@ -8,8 +8,69 @@
 
 #define GIMBAL_UNTETHERED
 
+//FEEDER CONSTANTS ---------------------------------------------------------------
+static constexpr CANBus FEED_BUS = CANBus::CAN_BUS1; //TODO: Need to check
+//
+static constexpr MotorID FEEDER_ID = MotorID::MOTOR7; //TODO: Need to check
+//
+static constexpr bool FEEDER_DIRECTION = false; //TODO: Need to check
+
+static constexpr SmoothPIDConfig FEEDER_VELOCITY_PID_CONFIG = {
+    .kp = 15.0f,
+    .ki = 0.0f,
+    .kd = 0.8f,
+    .maxICumulative = 10.0f,
+    .maxOutput = M2006_MAX_OUTPUT,
+    .tQDerivativeKalman = 1.0f,
+    .tRDerivativeKalman = 1.0f,
+    .tQProportionalKalman = 1.0f,
+    .tRProportionalKalman = 1.0f,
+    .errDeadzone = 0.0f,
+    .errorDerivativeFloor = 0.0f,
+};
+
+static constexpr float DEFAULT_BURST_LENGTH = 5;
+
+static constexpr float FEEDER_DEFAULT_RPM = 3000.0f;
+
+
+//SHOOTER CONSTANTS ------------------------------------------------------------------
+
+static constexpr CANBus SHOOTER_BUS = CANBus::CAN_BUS1; //TODO: Need to check
+//
+static constexpr MotorID SHOOTER_1_ID = MotorID::MOTOR3; //TODO: Need to check
+static constexpr MotorID SHOOTER_2_ID = MotorID::MOTOR4; //TODO: Need to check
+//
+static constexpr bool SHOOTER_1_DIRECTION = false; //TODO: Need to check
+static constexpr bool SHOOTER_2_DIRECTION = true; //TODO: Need to check
+
+static constexpr SmoothPIDConfig SHOOTER_VELOCITY_PID_CONFIG = {
+    .kp = 40.0f,
+    .ki = 0.10f,
+    .kd = 0.00f,
+    .maxICumulative = 10.0f,
+    .maxOutput = M3508_MAX_OUTPUT,
+    .tQDerivativeKalman = 1.0f,
+    .tRDerivativeKalman = 1.0f,
+    .tQProportionalKalman = 1.0f,
+    .tRProportionalKalman = 1.0f,
+    .errDeadzone = 0.0f,
+    .errorDerivativeFloor = 0.0f,
+};
+
+// clang-format off
+static constexpr uint16_t shooter_speed_array[6] = {
+    15, 3900,  // {ball m/s, flywheel rpm}
+    18, 4500,
+    30, 9000};
+// clang-format on
+
+static const Matrix<uint16_t, 3, 2> SHOOTER_SPEED_MATRIX(shooter_speed_array);
+
 static constexpr uint8_t SHOOTER_MOTOR_COUNT = 2;
 
+
+//GIMBAL CONSTANTS -----------------------------------------------------------------------
 /**
  * @brief GIMBAL SETUP
  */
@@ -178,64 +239,6 @@ static constexpr float kGRAVITY = 0.0f;
 static constexpr float HORIZON_OFFSET = -0.0f;
 // -------------------------------------------------------------------------------------------------------------------------
 
-static Vector3f IMU_MOUNT_POSITION{0.0f, 0.0f, 0.0f};
-
-static constexpr SmoothPIDConfig FEEDER_VELOCITY_PID_CONFIG = {
-    .kp = 15.0f,
-    .ki = 0.0f,
-    .kd = 0.8f,
-    .maxICumulative = 10.0f,
-    .maxOutput = M2006_MAX_OUTPUT,
-    .tQDerivativeKalman = 1.0f,
-    .tRDerivativeKalman = 1.0f,
-    .tQProportionalKalman = 1.0f,
-    .tRProportionalKalman = 1.0f,
-    .errDeadzone = 0.0f,
-    .errorDerivativeFloor = 0.0f,
-};
-
-static constexpr SmoothPIDConfig SHOOTER_VELOCITY_PID_CONFIG = {
-    .kp = 40.0f,
-    .ki = 0.10f,
-    .kd = 0.00f,
-    .maxICumulative = 10.0f,
-    .maxOutput = M3508_MAX_OUTPUT,
-    .tQDerivativeKalman = 1.0f,
-    .tRDerivativeKalman = 1.0f,
-    .tQProportionalKalman = 1.0f,
-    .tRProportionalKalman = 1.0f,
-    .errDeadzone = 0.0f,
-    .errorDerivativeFloor = 0.0f,
-};
-
-// clang-format off
-static constexpr uint16_t shooter_speed_array[6] = {
-    15, 3900,  // {ball m/s, flywheel rpm}
-    18, 4500,
-    30, 9000};
-// clang-format on
-
-static const Matrix<uint16_t, 3, 2> SHOOTER_SPEED_MATRIX(shooter_speed_array);
-
-static constexpr float FEEDER_DEFAULT_RPM = 3000.0f;
-
-static constexpr float DEFAULT_BURST_LENGTH = 5;
-
-// CAN Bus 1
-static constexpr CANBus SHOOTER_BUS = CANBus::CAN_BUS1;
-static constexpr CANBus FEED_BUS = CANBus::CAN_BUS1;
-
-//
-static constexpr MotorID FEEDER_ID = MotorID::MOTOR7;
-//
-static constexpr MotorID SHOOTER_1_ID = MotorID::MOTOR3;
-static constexpr MotorID SHOOTER_2_ID = MotorID::MOTOR4;
-
-static constexpr bool SHOOTER_1_DIRECTION = false;
-static constexpr bool SHOOTER_2_DIRECTION = true;
-
-static constexpr bool FEEDER_DIRECTION = false;
-
 static constexpr float GIMBAL_X_OFFSET = 0.0f;
 static constexpr float GIMBAL_Y_OFFSET = 0.0f;
 
@@ -274,6 +277,8 @@ static Vector3f BARREL_POSITION_FROM_GIMBAL_ORIGIN{
     0.01683f, //z = 0.01683
 };
 // clang-format on
+
+static Vector3f IMU_MOUNT_POSITION{0.0f, 0.0f, 0.0f};
 
 static constexpr size_t PROJECTILE_SPEED_QUEUE_SIZE = 10;
 
