78a27b4ddc28367a9e1dc139bd19eb54547448cc
Luis Luna <lunaluism@yahoo.com>
2023-10-04 22:06:32 -0500

    stop feeder

---
+++ aimbots-src/src/subsystems/feeder/stop_feeder_command.cpp
@@ -0,0 +1,26 @@
+#include "subsystems/feeder/stop_feeder_command.hpp"
+
+#ifdef FEEDER_COMPATIBLE
+namespace src::Feeder{
+StopFeederCommand::StopFeederCommand(src::Drivers* drivers, FeederSubsystem* feeder) {
+    this->drivers;
+    this->feeder;
+    addSubsystemRequirement(dynamic_cast<tap::control::Subsystem*>(feeder));
+}
+
+void StopFeederCommand::initialize() {
+}
+
+void StopFeederCommand::execute() {
+    feeder->setTargetRPM(0);
+}
+
+void StopFeederCommand::end(bool interrupted) {}
+
+bool StopFeederCommand::isReady() { return true; }
+
+bool StopFeederCommand::isFinished() const { return false; }
+
+};
+
+#endif // closes ifdef
---
+++ aimbots-src/src/subsystems/feeder/stop_feeder_command.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "tap/control/subsystem.hpp"
+#include "utils/common_types.hpp"
+#include "utils/robot_specific_inc.hpp"
+#include "drivers.hpp"
+#include "subsystems/feeder/feeder.hpp"
+
+#ifdef FEEDER_COMPATIBLE        // checks for the existence of macro definitions
+
+namespace src::Feeder {
+
+class StopFeederCommand : public TapCommand{
+public:
+    StopFeederCommand(src::Drivers* drivers, FeederSubsystem* Feeder);
+
+    void initialize() override;
+    void execute() override;
+    void end(bool interrupted) override;
+
+    bool isReady() override;
+    bool isFinished() const override;
+    const char* getName() const override { return "stop feeder command"; }
+
+private:
+    src::Drivers* drivers;
+    FeederSubsystem* feeder;
+};
+
+};
+
+
+#endif      // close ifdef
